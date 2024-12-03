2d601860cfe6bd81effc28975bf403b7a6926965
Chloe Tanghe <chloetanghedruez25@gmail.com>
2024-12-03 16:32:58 +0100

    frontend

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-//Install express server
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/assignement-app/"));
+// Mettez à jour le chemin ici pour inclure "browser"
+app.use(express.static(path.join(__dirname, 'dist', 'assignement-app', 'browser')));
 
-app.get("/*", function (req, res) {
- res.sendFile(path.join(__dirname + "/dist/assignement-app/index.html"));
+// Modifier la ligne suivante pour pointer vers le bon répertoire contenant index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'assignement-app', 'browser', 'index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8081);
+
+app.listen(process.env.PORT || 8081, () => {
+  console.log("Server is running on port 8081");
+});
