b62c3b304bb8e579ac881ad2e4c40e4013441909
Chloe Tanghe <chloetanghedruez25@gmail.com>
2024-12-03 18:51:24 +0100

    modif

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
+//Install express server
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-// Spécifiez le chemin vers le répertoire "browser" dans "dist/assignement-app"
-app.use(express.static(path.join(__dirname, 'dist', 'assignement-app', 'browser')));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/assignement-app/"));
 
-// Servir index.html à toutes les requêtes
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'assignement-app', 'browser', 'index.html'));
+app.get("/*", function (req, res) {
+ res.sendFile(path.join(__dirname + "/dist/assignement-app/index.html"));
 });
-
-// Utiliser le port fourni par Render ou 8081 si local
-const port = process.env.PORT || 8081;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8081);
